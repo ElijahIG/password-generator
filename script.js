df73afc1ff06466de16e9f1666abb8a520a68618
Elijah Davis <elijahgdavis1999@gmail.com>
2020-12-10 14:49:19 -0800

    Added letter shuffler for password

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
 
+function generatePassword() {
+  var password = [];
+
+  var passLength = prompt("Enter a password length:");
+  passLength = parseInt(passLength);
+
+  if (passLength < 8) {
+    alert("Password must have more than 7 characters");
+    return "";
+  }
+
+  if (passLength > 128) {
+    alert("Password must have less than 128 characters");
+    return "";
+  }
+
+  for (var i = 0; i < passLength; i++) {
+    var randomChar = lowercase[Math.floor(Math.random() * lowercase.length)];
+    password.push(randomChar);
+  }
+
+  return password.join("");
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
