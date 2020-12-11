3454384b87d47a75c770781d72bd04d43c11e712
Elijah Davis <elijahgdavis1999@gmail.com>
2020-12-10 17:46:29 -0800

    Added to javascript and readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# password-generator
+# Password-Generator
+
+Button that will prompt user how many charcters they would like in the password.
+
+Prompt will return user to start if they choose a amount lower than 8 or higher than 128.+
+
+The user will be prompted 4 times asking if they would like to include capital letters, lowercase letters, numbers, or special characters.
+
+The user will be shown a password on screen that matches all criteria.
--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
 
 function generatePassword() {
   var password = [];
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890";
+  var specialCharacters = "!@#$%&?/\\";
+  var charactersToGrab = "";
 
-  var passLength = prompt("Enter a password length:");
-  passLength = parseInt(passLength);
+  var passLength = parseInt(prompt("Enter a password length:"));
+
+  if (isNaN(passLength)) {
+    alert("Must be a number");
+    return "";
+  }
 
   if (passLength < 8) {
     alert("Password must have more than 7 characters");
@@ -18,8 +26,23 @@ function generatePassword() {
     return "";
   }
 
+  var upperQuestion = confirm("Do you want to include uppercase letters?");
+  if (upperQuestion) charactersToGrab += uppercase;
+
+  var lowerQuestion = confirm("Do you want to include lowercase letters?");
+  if (lowerQuestion) charactersToGrab += lowercase;
+
+  var numbersQuestion = confirm("Do you want to include numbers?");
+  if (numbersQuestion) charactersToGrab += numbers;
+
+  var specialQuestion = confirm("Do you want to include special characters?");
+  if (specialQuestion) charactersToGrab += specialCharacters;
+
+  console.log(charactersToGrab);
+
   for (var i = 0; i < passLength; i++) {
-    var randomChar = lowercase[Math.floor(Math.random() * lowercase.length)];
+    var randomChar =
+      charactersToGrab[Math.floor(Math.random() * charactersToGrab.length)];
     password.push(randomChar);
   }
 
@@ -27,9 +50,10 @@ function generatePassword() {
 }
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+  //passwordText.value = password;
+  document.querySelector("#password").value = generatePassword();
 }
 
 // Add event listener to generate button
