1a653796e755b536ccc8f724f3abd61e3475afd2
Elijah Davis <elijahgdavis1999@gmail.com>
2020-12-10 17:52:11 -0800

    Added image folder

--- script.js
+++ script.js
@@ -50,9 +50,6 @@ function generatePassword() {
 }
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-  //passwordText.value = password;
   document.querySelector("#password").value = generatePassword();
 }
 
